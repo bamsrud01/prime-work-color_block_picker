2b7739d5567c90e137c3ec0368e9af79264177a6
Barrett Amsrud <barrettamsrud@Barretts-MacBook-Pro.local>
2016-09-28 11:48:17 -0500

    Added divs, included basic functionality

---
+++ app.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+  createBlocks();
+
+  assignColors();
+
+  choose();
+
+  $('#blocks').on('click', '.pick', function() {
+      var $chosen = $(this).css('background-color');
+      console.log('Chosen: ' + $chosen);
+      console.log('To choose: ' + $('#color_select').css('color'));
+      if ($chosen === $('#color_select').css('color')) {
+        $('#response').text('That\'s right!');
+      } else {
+        $('#response').text('Sorry, that\'s not right.');
+      }
+      choose();
+
+  });
+
+});
+
+var colorList = ['red', 'yellow', 'blue', 'green', 'orange', 'purple', 'aqua',
+'magenta', 'tomato', 'pink', 'navy', 'olive', 'maroon', 'lime', 'salmon', 'brown'];
+
+var chooserArray = shuffleArray(colorList);
+
+function createBlocks() {
+  for ( var i = 0; i < 4; i++ ) {
+    $('#blocks').append('<div class="row"></div>');
+    for ( var j = 0; j < 4; j++ ) {
+      $('#blocks').children().last().append('<div class="pick" id="block' + ((4 * i) + j + 1) + '"></div>');
+    }
+  }
+}
+
+function assignColors() {
+  shuffleArray(colorList);
+  for (var i = 0; i < 16; i++) {
+    $('#block' + (i + 1)).css('background-color', colorList[i]);
+  }
+}
+//I take no credit for this function.  I found it on Stack Overflow, and it suited my needs.
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+function choose() {
+  if (chooserArray.length == 0) {
+    chooserArray = shuffleArray(colorList);
+  }
+  var current = chooserArray.pop();
+  $('#color_select').css('color', current);
+  $('#color_select').text(current.toUpperCase());
+}
